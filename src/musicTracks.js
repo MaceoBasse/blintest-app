f67026d512de5950233271fde7359e932a201c18
Maceo <basse.maceo@gmail.com>
2023-12-17 19:25:34 +0100

    Add author information to music tracks

--- src/musicTracks.js
+++ src/musicTracks.js
@@ -2,6 +2,7 @@ export const playlist1 = [
     {
         id: 1,
         title: "Shape of You",
+        author: "Ed Sheeran",
         source: "/music/Ed Sheeran - Shape of You.mp3",
     },
 
@@ -10,6 +11,7 @@ export const playlist2 = [
     {
         id: 2,
         title: "Despacito",
+        author: "Luis Fonsi",
         source: "/music/Luis Fonsi - Despacito ft. Daddy Yankee.mp3",
     }
 ];
