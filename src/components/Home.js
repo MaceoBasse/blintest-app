1955f822bf2bfa41e401e7a7629d255adeaa9357
Maceo <basse.maceo@gmail.com>
2023-12-18 09:39:55 +0100

    Refactor playlist buttons in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { popPlaylist, rapPlaylist,electroPlaylist } from '../musicTracks';
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
+  const playlists = [
+    { name: 'Pop', playlist: popPlaylist },
+    { name: 'Rap', playlist: rapPlaylist },
+    { name: 'Electro', playlist: electroPlaylist },
+  ];
   const navigate = useNavigate();
   const handlePlaylistClick = (playlist) => {
     navigate('/BlindTest', { state: { playlist: playlist } });
@@ -12,9 +17,11 @@ const Home = () => {
       <h1>BlindTest</h1>
       <p>Bienvenue sur notre application BlindTest!</p>
       <h1>Choisissez une playlist</h1>
-      <button onClick={() => handlePlaylistClick(popPlaylist)}>Playlist Pop</button>
-      <button onClick={() => handlePlaylistClick(rapPlaylist)}>Playlist Rap</button>
-      <button onClick={() => handlePlaylistClick(electroPlaylist)}>Playlist Electro</button>
+      {playlists.map((playlist) => (
+      <button key={playlist.name} onClick={() => handlePlaylistClick(playlist.playlist)}>
+        Playlist {playlist.name}
+      </button>
+    ))}
 
     </div>
   );
