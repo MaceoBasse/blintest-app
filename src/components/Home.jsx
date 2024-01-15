a92e45fb3a05dc7b7a0c12c4172e6be5715c9160
Maceo <basse.maceo@gmail.com>
2024-01-15 14:02:15 +0100

    Update button styling in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -73,12 +73,12 @@ function Home() {
                 </button>
               ))}
               <button
-                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-2 py-2 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                className="group inline-block rounded border border-indigo-600 bg-indigo-600 px-2 py-2 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
                 onClick={() => randomChoice()}
                 type="button"
               >
                 <svg
-                  className="fill-current text-white hover:text-indigo-600"
+                  className="fill-current text-white group-hover:text-indigo-600"
                   width="16px"
                   height="16px"
                   viewBox="0 0 32 32"
